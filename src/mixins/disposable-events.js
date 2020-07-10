7ba2ed74cc7ecb1a564fce6c502b3099e7db3c00
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-07-10 07:57:16 -0500

    feat: disposable-events import/export

--- src/mixins/disposable-events.js
+++ src/mixins/disposable-events.js
@@ -1,16 +1,9 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-let DisposableEvents;
-const Mixin = require('mixto');
-const {Disposable} = require('atom');
+import Mixin from 'mixto';
+import {Disposable} from 'atom';
 
-module.exports =
-(DisposableEvents = class DisposableEvents extends Mixin {
+export default class DisposableEvents extends Mixin {
   addDisposableEventListener(object, event, listener) {
     object.addEventListener(event, listener);
     return new Disposable(() => object.removeEventListener(event, listener));
   }
-});
+}
