13a17dcd27692dbf9fff1a429ee2161178d673de
aminya <aminyahyaabadi74@gmail.com>
2020-07-12 21:09:12 -0500

    feat: register-or-update-element
    - using optional chaining
    - using functions/lambdas
    - dynamic import grim to speed up loading
    - formatting

--- src/register-or-update-element.js
+++ src/register-or-update-element.js
@@ -1,11 +1,3 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
- */
-const {deprecate} = require('grim');
-
 if (global.__CUSTOM_HTML_ELEMENTS_CLASSES__ != null) {
   global.__ATOM_UTILS_CUSTOM_ELEMENT_CLASSES__ = global.__CUSTOM_HTML_ELEMENTS_CLASSES__;
   delete global.__CUSTOM_HTML_ELEMENTS_CLASSES__;
@@ -20,37 +12,42 @@ const callbackProperties = [
   'attributeChangedCallback'
 ];
 
-const decorateElementPrototype = function(target, source) {
-  callbackProperties.forEach(function(k) {
-    return Object.defineProperty(target, k, {
+function decorateElementPrototype(target, source) {
+  callbackProperties.forEach((k) => {
+    Object.defineProperty(target, k, {
       value() { return this[`__${k}`]?.apply(this, arguments); },
       writable: true,
       enumerable: true,
       configurable: true
-    });});
+    });
+  });
 
-  return Object.getOwnPropertyNames(source).forEach(function(k) {
+  Object.getOwnPropertyNames(source).forEach((k) => {
     if (['constructor'].includes(k)) { return; }
 
     const descriptor = Object.getOwnPropertyDescriptor(source, k);
     if (callbackProperties.indexOf(k) > -1) {
-      return Object.defineProperty(target, `__${k}`, descriptor);
+      Object.defineProperty(target, `__${k}`, descriptor);
     } else {
-      return Object.defineProperty(target, k, descriptor);
+      Object.defineProperty(target, k, descriptor);
     }
   });
-};
+}
 
-const decorateElementClass = (target, source) => Object.getOwnPropertyNames(source).forEach(function(k) {
-  if (['length', 'name', 'arguments', 'caller', 'prototype'].includes(k)) { return; }
+function decorateElementClass(target, source) {
+  Object.getOwnPropertyNames(source).forEach((k) => {
+    if (['length', 'name', 'arguments', 'caller', 'prototype'].includes(k)) { return; }
 
-  const descriptor = Object.getOwnPropertyDescriptor(source, k);
-  return Object.defineProperty(target, k, descriptor);
-});
+    const descriptor = Object.getOwnPropertyDescriptor(source, k);
+    Object.defineProperty(target, k, descriptor);
+  });
+}
 
-module.exports = function(nodeName, options) {
-  let elementClass, proto;
+let grim_deprecate; // dynamic imported
+
+export default function registerOrUpdateElements(nodeName, options) {
   const {class: klass} = options;
+  let proto;
   if (klass != null) {
     proto = klass.prototype;
   } else {
@@ -58,10 +55,21 @@ module.exports = function(nodeName, options) {
   }
 
   if (proto === options) {
-    deprecate('Using the prototype as the second argument is deprecated, use the prototype option instead');
+    const deprecationMessage = 'Using the prototype as the second argument is deprecated, use the prototype option instead'
+
+    if (!grim_deprecate) {
+      // dynamic imported for the first time
+      import('grim').then(({deprecate}) => {
+        grim_deprecate = deprecate;
+        grim_deprecate(deprecationMessage);
+      })
+    } else {
+      grim_deprecate(deprecationMessage);
+    }
+
   }
 
-
+  let elementClass;
   if (__ATOM_UTILS_CUSTOM_ELEMENT_CLASSES__[nodeName]) {
     elementClass = __ATOM_UTILS_CUSTOM_ELEMENT_CLASSES__[nodeName];
 
